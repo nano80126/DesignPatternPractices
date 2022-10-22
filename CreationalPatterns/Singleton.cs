dfc5ae66284d768eef2d396942dd1145642b700a
nano80126 <nano80126@gmail.com>
2022-10-22 21:19:53 +0800

    加入專案檔案。

---
+++ CreationalPatterns/Singleton.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternPractices.CreationalPatterns
+{
+    /// 
+    /// 確保一個系統只有實例化一個物件
+    /// 1. Greed Singleton
+    /// 2. Lazy Initialization
+    /// 3. Double Check Locking / DCL
+    /// 4. Using Lazy<T>
+    /// 若 Freight 不為園子操作，要加上 lock，否則多執行緒下，也有可能造成結果錯誤
+
+
+    /// <summary>
+    /// Greed Singleton
+    /// </summary>
+    class Supermarket
+    {
+        private int quantity = 100;
+        private static Supermarket uniqueInstance = new Supermarket();
+
+        public static Supermarket GetInstance => uniqueInstance;
+
+        /// <summary>
+        /// 建構涵式
+        /// </summary>
+        private Supermarket()
+        {
+
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            this.quantity = quantity;
+        }
+
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+    }
+
+    /// <summary>
+    /// Lazy initialization (非執行緒安全)
+    /// </summary>
+    public sealed class SupermarketLazy
+    {
+        private int quantity = 100;
+        private static SupermarketLazy uniqueInstance = null;
+
+        public static SupermarketLazy GetInstance
+        {
+            get
+            {
+                if (uniqueInstance == null)
+                {
+                    uniqueInstance = new SupermarketLazy();
+                    Console.WriteLine($"建立 SupermerketLazy");
+                }
+                return uniqueInstance;
+            }
+        }
+
+        /// <summary>
+        /// 建構涵式
+        /// </summary>
+        private SupermarketLazy()
+        {
+
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            this.quantity = quantity;
+        }
+
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+    }
+
+
+    /// <summary>
+    /// Lazy Initialization Double Check Locking
+    /// </summary>
+    public sealed class SupermarketLazyDCL
+    {
+        private int quantity = 100;
+        private static SupermarketLazyDCL uniqueInstance = null;
+        private static readonly object _lock = new object();
+
+        public static SupermarketLazyDCL GetInstance
+        {
+            get
+            {
+                if (uniqueInstance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (uniqueInstance == null)
+                        {
+                            uniqueInstance = new SupermarketLazyDCL();
+                            Console.WriteLine($"建立 SupermerketLazy");
+                        }
+                    }
+                }
+                return uniqueInstance;
+            }
+        }
+
+        /// <summary>
+        /// 建構涵式
+        /// </summary>
+        private SupermarketLazyDCL()
+        {
+
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            this.quantity = quantity;
+        }
+
+        public int GetQuantity()
+        {
+            return this.quantity;
+        }
+    }
+
+    /// <summary>
+    /// Lazy Initialization Using Lazy<T>
+    /// </summary>
+    public sealed class SupermarketLazy2
+    {
+        private int _quantity;
+
+        private SupermarketLazy2() { }
+
+        private static readonly Lazy<SupermarketLazy2> lazy = new Lazy<SupermarketLazy2>(() =>
+        {
+            SupermarketLazy2 instance = new SupermarketLazy2()
+            {
+                _quantity = 100
+            };
+            return instance;
+        });
+
+        public static SupermarketLazy2 Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            _quantity = quantity;
+        }
+
+        public int GetQuantity()
+        {
+            return _quantity;
+        }
+    }
+
+    class Freight
+    {
+        public SupermarketLazyDCL supermarket;
+
+        private static readonly object _lock = new object();
+        public Freight(SupermarketLazyDCL supermarket)
+        {
+            this.supermarket = supermarket;
+        }
+
+        public void MoveIn(int i)
+        {
+            lock (_lock)
+            {
+                supermarket.SetQuantity(supermarket.GetQuantity() + i); 
+            }
+        }
+
+        public void MoveOut(int i)
+        {
+            supermarket.SetQuantity(supermarket.GetQuantity() - i);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DesignPatternPractices.CreationalPatterns;
+
+namespace DesignPatternPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //RunSingletonThread();
+            RunSingletonTask();
+
+            Console.WriteLine("Please enter any key to leave.");
+            Console.ReadKey();
+        }
+
+        #region 單例模式
+        /// <summary>
+        /// 使用Thread 執行單例模式
+        /// </summary>
+        private static void RunSingletonThread()
+        {
+            //SupermarketLazyDCL supermarketLazyDcl1 = SupermarketLazyDCL.GetInstance;
+            //SupermarketLazyDCL supermarketLazyDcl2 = SupermarketLazyDCL.GetInstance;
+
+            Thread th = new Thread(() =>
+            {
+                SupermarketLazyDCL supermarketLazyDcl1 = SupermarketLazyDCL.GetInstance;
+                Freight freight = new Freight(supermarketLazyDcl1);
+                freight.MoveIn(30);
+                Console.WriteLine($"1 號已送達！商品數量 {freight.supermarket.GetQuantity()}");
+            });
+
+            Thread th2 = new Thread(() =>
+            {
+                SupermarketLazyDCL supermarketLazyDcl2 = SupermarketLazyDCL.GetInstance;
+                Freight freight = new Freight(supermarketLazyDcl2);
+                freight.MoveIn(40);
+                Console.WriteLine($"2 號已送達！商品數量 {freight.supermarket.GetQuantity()}");
+            });
+
+            th.Start();
+            th2.Start();
+
+            th.Join();
+            th2.Join();
+        }
+
+        /// <summary>
+        /// 使用 Task 執行單例模式
+        /// </summary>
+        private static void RunSingletonTask()
+        {
+            Task t1 = Task.Run(() =>
+            {
+                SupermarketLazyDCL supermarketLazyT = SupermarketLazyDCL.GetInstance;
+                Freight freight = new Freight(supermarketLazyT);
+                freight.MoveIn(50);
+                Console.WriteLine($"3 號已送達！商品數量 {freight.supermarket.GetQuantity()}");
+            });
+
+            Task t2 = Task.Run(() =>
+            {
+                SupermarketLazyDCL supermarketLazyT = SupermarketLazyDCL.GetInstance;
+                Freight freight = new Freight(supermarketLazyT);
+                freight.MoveIn(60);
+                Console.WriteLine($"4 號已送達！商品數量 {freight.supermarket.GetQuantity()}");
+            });
+
+
+            Task.WaitAll(t1, t2);
+        }
+        #endregion
+    }
+}
